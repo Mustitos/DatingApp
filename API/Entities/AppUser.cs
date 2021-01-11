d9cf2c671ed17132b529d866edbafea7dc3379c3
Marc <m.marc.c@hotmail.com>
2021-01-10 21:53:14 -0500

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,8 +3,10 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
-        public string UserName { get; set; } 
+        public string Username { get; set; } 
 
-        
+        public byte[] PasswordHast { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
+
 }
